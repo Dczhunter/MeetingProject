1c341c661b8772d8868e24cda58258894b1c5663
dingcz <651964875@qq.com>
2019-08-16 16:06:33 +0800

    框架改造

---
+++ src/components/MeetingModule/Md_DutyThings/Md_DutyThings.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="Page">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.Page{
+  display: flex;
+  width: 100%;
+  height: 100%;
+  align-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #ffe4c4;
+}
+
+</style>
---
+++ src/components/MeetingModule/Md_SubmitThings/Md_SubmitThings.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="Page">
+        <div class="SearchRegion"> search</div>
+        <div class="DataRegion">data</div>
+        <div class="FunctionRegion">func</div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.Page{
+  display: flex;
+  height: 100%;
+  width: 100%;
+  align-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #1d5e0d;
+}
+.SearchRegion{
+    display: flex;
+    width: 100%;
+    height: 10%;
+    background-color: #aa901d
+}
+
+.DataRegion{
+    display: flex;
+    width: 100%;
+    height: 80%;
+    background-color: #aa901d
+}
+.FunctionRegion{
+    display: flex;
+    width: 100%;
+    height: 10%;
+    background-color: #30290b
+}
+</style>
+
+
